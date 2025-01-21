e5566b4b0f95eb7b8fadd2fc56e7eb086f7c6b08
Marina <131159001+marredon9@users.noreply.github.com>
2025-01-21 12:04:50 +0100

    Add files via upload

---
+++ U8/src/U8/PruebaPila.java
@@ -0,0 +1,15 @@
+package U8;
+
+public class PruebaPila{
+	
+	public void rellenar(ColeccionInterfaz coleccion) {
+		
+	}
+	
+	public void imprimirYVaciar(ColeccionInterfaz coleccion) {
+		
+	}
+
+
+	
+}
